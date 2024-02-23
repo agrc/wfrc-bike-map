8ec9bb39ca6b160afe6b3ce35e503c464b4fdecc
stdavis <stdavis@utah.gov>
2024-02-23 09:29:34 -0700

    feat: implement tailwind

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import '@esri/calcite-components/dist/calcite/calcite.css';
 import { defineCustomElements } from '@esri/calcite-components/dist/loader';
+import './src/index.css'
 
 defineCustomElements(window, {
   resourcesUrl: '/esri-calcite/assets',
