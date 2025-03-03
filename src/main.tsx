9f55a0e4bc78109248293b77519140e7c0b6e546
stdavis <stdavis@utah.gov>
2025-03-03 14:41:51 -0700

    chore: add error boundary
    closes #70

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import {
 } from '@ugrc/utah-design-system';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 import App from './App';
 import './index.css';
 
@@ -24,12 +25,23 @@ if (import.meta.env.VITE_FIREBASE_CONFIG) {
 
 esriConfig.assetsPath = './assets';
 
+const ErrorFallback = ({ error }: { error: Error }) => {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: 'red' }}>{error.message}</pre>
+    </div>
+  );
+};
+
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <FirebaseAppProvider config={firebaseConfig}>
-      <FirebaseAnalyticsProvider>
-        <App />
-      </FirebaseAnalyticsProvider>
-    </FirebaseAppProvider>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <FirebaseAppProvider config={firebaseConfig}>
+        <FirebaseAnalyticsProvider>
+          <App />
+        </FirebaseAnalyticsProvider>
+      </FirebaseAppProvider>
+    </ErrorBoundary>
   </React.StrictMode>,
 );
