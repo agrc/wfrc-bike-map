db149e96a9e0c53e347a0ffec68f04a4a798f0dc
stdavis <stdavis@utah.gov>
2025-04-02 09:41:27 -0600

    feat(functions): send confirmation and notification feedback emails
    #17

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import { addFeatures } from '@esri/arcgis-rest-feature-service';
 import { ApplicationCredentialsManager } from '@esri/arcgis-rest-request';
+import sgMail from '@sendgrid/mail';
 import * as logger from 'firebase-functions/logger';
 import { defineSecret, defineString } from 'firebase-functions/params';
 import { HttpsError, onCall } from 'firebase-functions/v2/https';
@@ -8,12 +9,19 @@ import type {
   FeedbackSubmission,
 } from '../../shared/types.js';
 
+// secrets
 const clientId = defineSecret('AGOL_CLIENT_ID');
 const clientSecret = defineSecret('AGOL_CLIENT_SECRET');
+const sendgridApiKey = defineSecret('SENDGRID_API_KEY');
+
+// environment variables
 const pointFeatureService = defineString('POINT_FEATURE_SERVICE');
 const lineFeatureService = defineString('LINE_FEATURE_SERVICE');
+const feedbackEmail = defineString('FEEDBACK_EMAIL');
 
 let appManager: ApplicationCredentialsManager;
+
+const NO_REPLY_EMAIL = 'noreply@utah.gov';
 
 export const submitFeedback = onCall<
   FeedbackSubmission,
@@ -21,7 +29,7 @@ export const submitFeedback = onCall<
 >(
   {
     cors: [/bikeways(\.dev)?\.utah\.gov/],
-    secrets: [clientId, clientSecret],
+    secrets: [clientId, clientSecret, sendgridApiKey],
   },
   async ({ data }) => {
     logger.info('adding new feature to feature service', data);
@@ -33,11 +41,15 @@ export const submitFeedback = onCall<
       });
     }
 
+    sgMail.setApiKey(sendgridApiKey.value());
+
     let response;
+    let featureServiceUrl;
     if (data.feature.geometry && 'x' in data.feature.geometry) {
       // point
+      featureServiceUrl = pointFeatureService.value();
       response = await addFeatures({
-        url: pointFeatureService.value(),
+        url: featureServiceUrl,
         authentication: appManager,
         features: [
           {
@@ -51,8 +63,10 @@ export const submitFeedback = onCall<
         ],
       });
     } else {
+      // line
+      featureServiceUrl = lineFeatureService.value();
       response = await addFeatures({
-        url: lineFeatureService.value(),
+        url: featureServiceUrl,
         authentication: appManager,
         features: [
           {
@@ -79,6 +93,49 @@ export const submitFeedback = onCall<
     }
 
     logger.info('Feature added successfully', response.addResults[0]);
+
+    if (data.email) {
+      try {
+        await sgMail.send({
+          to: data.email,
+          from: NO_REPLY_EMAIL,
+          templateId: 'd-fabcd56c0cbe401a82909750847711a7',
+          dynamicTemplateData: {
+            feedback: data.feedback,
+            name: data.name,
+            url: data.url,
+          },
+        });
+      } catch (error) {
+        logger.error('Failed to send email', error);
+        throw new HttpsError('internal', 'Failed to send email');
+      }
+      logger.info('Feedback confirmation email sent successfully');
+    }
+
+    try {
+      await sgMail.send({
+        to: feedbackEmail.value(),
+        from: NO_REPLY_EMAIL,
+        templateId: 'd-8d93857b80bc4d0292c22863a85d6763',
+        dynamicTemplateData: {
+          feedback: data.feedback,
+          email: data.email,
+          name: data.name,
+          url: data.url,
+          layer: data.layer,
+          attributes: JSON.stringify(data.feature.attributes, null, 2),
+          feedbackRecord: {
+            url: featureServiceUrl,
+            objectId: response.addResults[0].objectId,
+            globalId: response.addResults[0].globalId,
+          },
+        },
+      });
+    } catch (error) {
+      logger.error('Failed to send feedback notification email', error);
+      throw new HttpsError('internal', 'Failed to send notification email');
+    }
 
     return { success: true };
   },
